7911a2ac0a2dc8e966506eadc3a2335f8150c494
Josh Davis <josh@joshldavis.com>
2013-09-03 11:11:29 -0500

    Add homework files for hw02

---
+++ cs342/homework02/hw02-answer-sheet.rkt
@@ -0,0 +1,164 @@
+#lang racket
+(#%provide (all-defined))
+
+#|
+If there are any specific instructions for a problem, please read them carefully. Otherwise,
+follow these general rules:
+   - replace the 'UNIMPLEMENTED symbol with your solution
+   - you are NOT allowed to change the names of any definition
+   - you are NOT allowed to change the number of arguments of the pre-defined functions,
+     but you can change the names of the arguments if you deem it necessary.
+   - make sure that you submit an asnwer sheet that compiles! If you cannot write
+     a correct solution at least make it compile, if you cannot make it compile then
+     comment it out. In the latter case, make sure that the default definitions
+     for the problem are still present. Otherwise you may be penalized up to 25%
+     of the total points for the homework.
+   - you may use any number of helper functions you deem necessary.
+
+When done, make sure that you do not get any errors when you hit the "Run" button. You will
+lose up to 25% of the total points for the entire homework depending on the number of errors.
+If you cannot come up with a correct solution then please make the answer-sheet
+compile correctly and comment any partial solution you might have; if this is the case,
+the default definitions will have to be present!
+
+|#
+;======================================01=======================================
+;((3 + 3) * 9)
+;equal to 54
+(define (p1-1)
+  'UNIMPLEMENTED
+)
+
+;((6 * 9) / ((4 + 2) + (4 * 3)))
+;equal to 3
+(define (p1-2)
+  'UNIMPLEMENTED
+)
+
+;(2* ((20 - (91 / 7)) * (45 - 42)))
+;equal to 42
+(define (p1-3)
+  'UNIMPLEMENTED
+)
+;======================================02=======================================
+;write your answer as a string; you do not need to write any special escape
+;characters to distinguish new lines.
+(define p2
+  "NO
+    ANSWER"
+)
+;======================================03=======================================
+;;Write the definitions of x,y,z here:
+
+;======================================04=======================================
+;you will need to have solved problem 3. The values x,y,z are not parameters
+;of this function!
+(define (p4)
+  'UNIMPLEMENTED
+)
+
+;======================================05=======================================
+(define (p5)
+  'UNIMPLEMENTED
+)
+
+;======================================06=======================================
+(define (p6)
+  'UNIMPLEMENTED
+)
+
+;======================================07=======================================
+;same instructions as problem 02.
+(define p7
+  "NO
+    ANSWER"
+)
+
+;======================================08=======================================
+(define p8
+  "NO
+    ANSWER"
+)
+
+;======================================09=======================================
+(define p9
+  "NO
+    ANSWER"
+)
+
+;======================================10=======================================
+(define p10
+  "NO
+    ANSWER"
+)
+
+;======================================11=======================================
+;(4 2 6 9)
+(define (p11-1)
+  'UNIMPLEMENTED
+)
+
+;(spaceship
+;  (name(serenity))
+;  (class(firefly)))
+(define (p11-2)
+  'UNIMPLEMENTED
+)
+
+;(2 * ((20 - (91 / 7)) * (45 - 42)))
+(define (p11-3)
+  'UNIMPLEMENTED
+)
+
+;======================================12=======================================
+(define example '(a b c))
+
+;(d a b c)
+(define (p12-1 lst)
+  'UNIMPLEMENTED
+)
+
+;(a b d a b)
+(define (p12-2 lst)
+  'UNIMPLEMENTED
+)
+
+;(b c d a)
+(define (p12-3 lst)
+  'UNIMPLEMENTED
+)
+
+
+;======================================13=======================================
+(define p13
+  "NO
+    ANSWER"
+)
+
+;======================================14=======================================
+(define (create-error-msg sym val)
+  'UNIMPLEMENTED
+)
+;======================================15=======================================
+(define (check-correctness pair)
+  'UNIMPLEMENTED
+)
+
+;======================================16=======================================
+;No answer necessary
+;======================================17=======================================
+;;write two examples that fail *only* at runtime here:
+
+(define (p17-example-1)
+  'UNIMPLEMENTED
+)
+
+(define (p17-example-2)
+  'UNIMPLEMENTED
+)
+
+;===================================18===========================================
+
+(define (pascal n)
+ 'UNIMPLEMENTED
+)
---
+++ cs342/homework02/hw02-tests.rkt
@@ -0,0 +1,82 @@
+#lang racket
+(#%provide (all-defined))
+(#%require "test-infrastructure.rkt")
+(#%require rackunit)
+
+;if you rename the answer sheet to conform with the submission requirements
+;you will have to change this line so that the tests can find the
+(#%require "hw02-answer-sheet.rkt")
+
+;to run the tests, evaluate:
+;>(test all)
+
+(define all
+  (test-suite
+    "These are all the tests for homework 2"
+
+    (342-check-equal? (p1-1) 54 "Problem 1, ((3 + 3) * 9)")
+    (342-check-equal? (p1-2) 3 "Problem 1, ((6 * 9) / ((4 + 2) + (4 * 3)))")
+    (342-check-equal? (p1-3) 42 "Problem 1, (2* ((20 - (91 / 7)) * (45 - 42)))")
+
+    (342-check-equal? (p4) 7 "p2, sum of the two largest x,y,z")
+    (342-check-equal? (p5) 5 "p2, sum of the two smallest x,y,z")
+    (342-check-equal? (p6) #f "p2, x, y are not equal")
+
+    (342-check-equal? (p12-1 example) '(d a b c) "12-1")
+    (342-check-equal? (p12-2 example) '(a b d a b) "12-2, ")
+    (342-check-equal? (p12-3 example) '(b c d a) "12-3")
+
+    ;;problem 14
+    (342-check-equal? (create-error-msg 'forty-two 42)
+                      "This is a custom error message we will be using next. Symbol 'forty-two was not paired with value 42")
+
+    ;;problem 15
+    (342-check-equal? (check-correctness '(answer-to-everything 42)) #t)
+    (342-check-equal? (check-correctness '(symbol-other-than-the-previous-one 42)) #f)
+    (342-check-equal? (check-correctness '(test 30)) #f)
+
+    ;this will check to see if an exception with the below written error-message
+    ;is raised.
+    (342-check-exn
+       (check-correctness '(answer-to-everything 10))
+       "This is a custom error message we will be using next. Symbol 'answer-to-everything was not paired with value 42")
+
+
+    ;;problem 18
+
+    (342-check-equal?
+       (pascal 1)
+       '((1))
+     )
+
+    (342-check-equal?
+       (pascal 2)
+       '((1) (1 1))
+     )
+
+    (342-check-equal?
+       (pascal 3)
+       '((1) (1 1) (1 2 1))
+     )
+
+    (342-check-equal?
+       (pascal 4)
+       '((1) (1 1) (1 2 1) (1 3 3 1))
+     )
+
+    (342-check-equal?
+       (pascal 5)
+       '((1) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1))
+     )
+
+    (342-check-equal?
+       (pascal 6)
+       '((1) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1) (1 5 10 10 5 1))
+     )
+
+    (342-check-equal?
+       (pascal 7)
+       '((1) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1) (1 5 10 10 5 1) (1 6 15 20 15 6 1))
+     )
+    )
+)
