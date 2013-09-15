29cbdc6265b7c01a33981b77a2ce3fefcf044a24
Josh Davis <josh@joshldavis.com>
2013-09-15 12:40:42 -0500

    Commit homework 4 files

---
+++ cs342/homework04/hw04-answer-sheet.rkt
@@ -0,0 +1,76 @@
+#lang racket
+(#%provide (all-defined))
+
+#|
+IMPORTANT:
+Overall, you are allowed to change this file in any way that does *not* affect the
+compilation of the accompanying test file. Changes that are almost certain to break
+the above restriction are:
+  - changing the names of any definitions that are explicitely used in the tests
+    (e.g. function names, relevant constants)
+
+If there are any specific instructions for a problem, please read them carefully. Otherwise,
+follow these general rules:
+
+   - replace the 'UNIMPLEMENTED symbol with your solution
+   - you are NOT allowed to change the number of arguments of the pre-defined functions,
+     because changing the number of arguments automatically changes the semantics of the 
+     function. Changing the name of the arguments is permitted since that change only
+     affects the readability of the function, not the semantics.
+   - you may write any number of helper functions
+
+When done, make sure that the accompanying test file compiles. You will lose up to 25%
+of the total points for the entire homework depending on the number of errors.
+If you cannot come up with a correct solution then please make the answer-sheet
+compiles. If you have partial solutions that do not compile please comment them out,
+if this is the case, the default definitions will have to be present since the tests
+will be expecting functions with the names defined here.
+
+Submission guidelines:
+   - please rename the file to hw04-yourlastname.rkt prior to submission
+   - only this file needs to be uploaded to the blackboard system
+|#
+
+;======================================01=======================================
+(define (foldl-342 op zero-el lst)
+   'UNIMPLEMENTED  
+)
+
+;---
+(define (foldr-342 op zero-el lst)
+   'UNIMPLEMENTED  
+)
+
+;======================================02=======================================
+(define (andmap-342 test-op lst)
+  'UNIMPLEMENTED
+)
+
+;======================================03=======================================
+(define (map-reduce m-op r-op zero-el lst)
+  'UNIMPLEMENTED
+)
+
+;======================================04=======================================
+(define (matrix-to-vector op mat)
+  'UNIMPLEMENTED  
+)
+
+;======================================05=======================================
+(define (change-at-index i new-el lst)
+  'UNIMPLEMENTED
+)
+
+;======================================06=======================================
+(define-syntax-rule (for {var in value-range} return result)
+  'UNIMPLEMENTED
+)
+;======================================07=======================================
+(define-syntax-rule (seq expr1 expr2)
+  'UNIMPLEMENTED
+)
+
+;====
+(define-syntax-rule (while condition body)
+  'UNIMPLEMENTED
+)
---
+++ cs342/homework04/hw04-tests.rkt
@@ -0,0 +1,300 @@
+#lang racket
+(#%provide (all-defined))
+(#%require "test-infrastructure.rkt")
+(#%require rackunit)
+
+(#%require "hw04-answer-sheet.rkt")
+
+;as you can probably figure out, this function will run
+;all the tests.
+(define (test-all)
+  (test p1-a)
+  (test p1-b)
+  (test p2)
+  (test p3)
+  (test p4)
+  (test p5)
+  (test p6)
+  (test p7-a)
+  (test p7-b)
+)
+
+;======================================01=======================================
+(define p1-a
+  (test-suite
+    "fold left"
+    (342-check-equal?
+       (foldl-342 + 0 '(1 2 3 4))
+       10
+       "foldl with addition"
+     )
+    
+    (342-check-equal?
+       (foldl-342 + 0 '())
+       0
+       "foldl over an empty list"
+     )
+    
+    (342-check-equal?
+       (foldl-342 + 42 '())
+       42
+       "foldl over an empty list with zero element = 42"
+     )
+    
+    (342-check-equal?
+       (foldl-342 string-append "" (list "!!!" "42"  "is " "answer " "the " ))
+       "the answer is 42!!!"
+       "foldl over strings"
+     )
+    
+    (342-check-equal?
+       (foldl-342 - 0 '(1 2 3 4))
+        2
+       "foldl with subtraction in order list"
+     )
+    
+    (342-check-equal?
+       (foldl-342 - 0 '(4 3 2 1))
+        -2
+       "foldl with subtraction reverse order list"
+     )
+  )
+)
+;---
+
+(define p1-b
+  (test-suite
+    "fold right"
+    (342-check-equal?
+       (foldr-342 + 0 '(1 2 3 4))
+       10
+       "foldr with addition"
+     )
+    
+    (342-check-equal?
+       (foldr-342 + 0 '())
+       0
+       "foldr over an empty list"
+     )
+    
+    (342-check-equal?
+       (foldr-342 + 42 '())
+       42
+       "foldr over an empty list with zero element = 42"
+     )
+    
+    (342-check-equal?
+       (foldr-342 string-append "" (list "the " "answer " "is " "42" "!!!"))
+       "the answer is 42!!!"
+       "foldr over strings"
+     )
+    
+    (342-check-equal?
+       (foldr-342 - 0 '(1 2 3 4))
+        -2
+       "foldr with subtraction in order list"
+     )
+    
+    (342-check-equal?
+       (foldr-342 - 0 '(4 3 2 1))
+        2
+       "foldr with subtraction reverse order list"
+     )
+  )
+)
+;======================================02=======================================
+(define p2
+  (test-suite
+    "andmap"
+    (342-check-equal?
+      (andmap-342 odd? '(1 3 5))
+      #t
+      "list containing only odd numbers"
+   )
+    
+    (342-check-equal?
+      (andmap-342 odd? '(1 3 6))
+      #f
+      "list of odds containing one even number"
+    )
+    
+    (342-check-equal?
+      (andmap-342 odd? '())
+      #t
+      "empty list"
+    )
+    
+  )
+)
+
+;======================================03=======================================
+(define add-forty-two
+  (lambda (x) (+ x 42)))
+
+(define p3
+  (test-suite
+    "map reduce"
+    
+    (342-check-equal?
+      (map-reduce add-forty-two + 0 '(0 1 2))
+      129
+      "we add 42 to each number then add them together"
+    )
+    
+    (342-check-equal?
+      (map-reduce add-forty-two + 0 '())
+      0
+      "map reduce over an empty list with zero element 0"
+    )
+    
+    (342-check-equal?
+      (map-reduce add-forty-two + 42 '())
+      42
+      "map reduce over an empty list with zero element = 42"
+    )
+  )
+)
+
+;======================================04=======================================
+(define n-matrix 
+   '((1 2 3 4)
+     (5 6 7 8)
+     (9 0 1 2))
+)
+
+(define str-matrix
+  '(("a" "c" "e")
+    ("b" "d" "f"))
+)
+
+(define p4
+  (test-suite
+    "matrix to vector"
+    
+    (342-check-equal?
+      (matrix-to-vector + n-matrix)
+      '(15 8 11 14)
+    )
+    
+    (342-check-equal?
+      (matrix-to-vector string-append str-matrix)
+      '("ab" "cd" "ef")
+    )
+  )
+)
+;======================================05=======================================
+(define p5
+  (test-suite
+    "change-at-index"
+    
+    (342-check-equal?
+      (change-at-index 0 42 '(please do not replace me))
+      '(42 do not replace me)
+    )
+    
+    (342-check-equal?
+      (change-at-index 1 42 '(please do not replace me))
+     '(please 42 not replace me)
+    )
+    
+    (342-check-equal?
+      (change-at-index 2 42 '(please do not replace me))
+      '(please do 42 replace me)
+    )
+    
+    (342-check-equal?
+      (change-at-index 3 42 '(please do not replace me))
+      '(please do not 42 me)
+    )
+    
+    (342-check-equal?
+      (change-at-index 4 42 '(please do not replace me))
+      '(please do not replace 42)
+    )
+  )
+)
+;======================================06=======================================
+(define p6
+  (test-suite
+    "for"
+    
+    (342-check-equal?
+       (for {i in '()} return (+ i 42))
+       '()
+       "empty list"
+     )
+    
+    (342-check-equal?
+       (for {a in '(0 1 2 3)} return (+ a 42))
+       '(42 43 44 45)
+       "add 42 to the list"
+     
+     )
+    
+    (342-check-equal?
+       (for {to-ignore in '(0 1 2 3)} return 42)
+       '(42 42 42 42)
+       "ignore looping variable"
+     )
+  )
+)
+;======================================07=======================================
+(define test-var-1 42)
+(define p7-a
+  (test-suite
+    "seq"
+    
+    (342-check-true
+       (and 
+          (equal? (seq (set! test-var-1 'not-42) 342) 342)
+          (equal? test-var-1 'not-42))
+       "measure side effect and return value"
+     )
+    
+    (342-check-equal?
+       (seq 'to-be-ignored (* 5 6))
+       30
+       "ignoring first expression"
+     )
+  )
+)
+;;======================
+(define test-counter-1 0)
+
+(define p7-b
+  (test-suite
+    "while"
+    
+    (342-check-true
+       (and
+         (equal? 0 (while (< test-counter-1 100)
+                          (set! test-counter-1 (+ test-counter-1 1))
+                   )
+         )
+         (equal? test-counter-1 100)
+       )
+       "loop and increment counter, while always returns 0"
+     )
+    
+    (test-case
+       "nested whiles"
+       (let ([i 0] [j 0] [v #()])
+         (while (< i 3)
+           (begin
+              ;;this condition is here to exemplify that it should be arbitrarily complex
+              (while (and (< j 5) (= 1 1))
+                  (begin 
+                    (set! j (+ j 1))
+                    (set! v (vector-append v #(*)))
+                  )
+              )
+              (set! v (vector-append v #(/n)))
+              (set! j 0)
+              (set! i (+ i 1))
+           )
+         )
+         (342-check-equal? v #(* * * * * /n * * * * * /n * * * * * /n))
+       )
+     )
+  )
+)
