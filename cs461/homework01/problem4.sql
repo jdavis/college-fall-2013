970381087a3f0759811eb27194200645e602ffa3
Josh Davis <josh@joshldavis.com>
2013-09-24 08:51:10 -0500

    Add part of problem 4 solution

---
+++ cs461/homework01/problem4.sql
@@ -0,0 +1,18 @@
+# Part A
+SELECT;
+
+# Part B
+SELECT;
+
+# Part C
+SELECT *
+FROM Movie m1, Movie m2
+WHERE m1.title = 'Gone With the Wind'
+AND m2.length > m1.length;
+
+# Part D
+SELECT *
+FROM MovieExec m1, MovieExec m2
+WHERE m1.name = 'Merv Griffin'
+AND m2.netWorth > m1.netWorth;
+
