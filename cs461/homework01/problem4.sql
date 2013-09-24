81d6bd5559ab118c83a9775b877eed150d2f9310
Josh Davis <josh@joshldavis.com>
2013-09-24 11:21:47 -0500

    Finish problem 4

--- cs461/homework01/problem4.sql
+++ cs461/homework01/problem4.sql
@@ -1,18 +1,31 @@
-# Part A
-SELECT;
+/*
+ * Part A
+ */
+SELECT ms.name
+FROM StarsIn s, MovieStar ms
+WHERE s.movieTitle = 'Terms of Endearment'
+AND ms.name = s.starName AND ms.gender = 'M';
 
-# Part B
-SELECT;
+/*
+ * Part B
+ */
+SELECT s.starName
+FROM StarsIn s, Movie m
+WHERE m.studioName = 'MGM' AND m.year = 1995
+AND s.movieTitle = m.title AND s.movieYear = m.year;
 
-# Part C
-SELECT *
+/*
+ * Part C
+ */
+SELECT m2.title
 FROM Movie m1, Movie m2
 WHERE m1.title = 'Gone With the Wind'
 AND m2.length > m1.length;
 
-# Part D
-SELECT *
+/*
+ * Part D
+ */
+SELECT m2.name
 FROM MovieExec m1, MovieExec m2
 WHERE m1.name = 'Merv Griffin'
 AND m2.netWorth > m1.netWorth;
-
