dd8b0d4122734c0871649340ca1a833a7c9548cb
Josh Davis <josh@joshldavis.com>
2013-09-24 23:13:10 -0500

    Finish problem 2

--- cs461/homework01/createtbl.sql
+++ cs461/homework01/createtbl.sql
@@ -1,58 +1,41 @@
+/*
+ * Entities.
+ */
+
 CREATE TABLE `airport` (
 	`code` INT,
 	`name` VARCHAR(64),
 	`city` VARCHAR(64),
 	`state` VARCHAR(16),
+
 	PRIMARY KEY (`code`)
 );
 
 CREATE TABLE `airplane_type` (
 	`name` VARCHAR(64),
 	`max_seats` INT,
-	`company` VARCHAR(128),
-	PRIMARY KEY (`name`),
-	FOREIGN KEY (`airport_code`)
-		REFERENCES `airport` (`code`)
-);
+	`company` VARCHAR(64),
 
-CREATE TABLE `can_land` (
-	`airplane_type_name` VARCHAR(64),
-	`airport_code` INT,
-	PRIMARY KEY (`airport_type_name`, `airport_code`),
-	FOREIGN KEY (`airport_code`)
-		REFERENCES `airport`,
-	FOREIGN KEY (`airplane_type_name`)
-		REFERENCES `airplane_type`
+	PRIMARY KEY (`name`)
 );
 
 CREATE TABLE `airplane` (
 	`id` INT,
 	`total_seats` INT,
-	`type` VARCHAR(64) NOT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`type`)
-		REFERENCES `airplane_type`
+
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `arrival_airport` (
-	`airport_code` INT,
-	`scheduled_arrival_time` DATETIME,
-	FOREIGN KEY (`airport_code`)
-		REFERENCES `airport`
-);
+CREATE TABLE `flight_leg` (
+	`number` INT,
 
-
-
-CREATE TABLE `departure_airport` (
-	`airport_code` INT,
-	`scheduled_departure_time` DATETIME,
-	FOREIGN KEY (`airport_code`)
-		REFERENCES `airport`
+	PRIMARY KEY (`number`)
 );
 
 CREATE TABLE `flight` (
 	`number` INT,
 	`airline` VARCHAR(64),
+
 	PRIMARY KEY (`number`)
 );
 
@@ -60,9 +43,184 @@ CREATE TABLE `fare` (
 	`code` INT,
 	`amount` DOUBLE,
 	`restrictions` VARCHAR(64),
+
+	PRIMARY KEY (`code`)
+);
+
+CREATE TABLE `leg_instance` (
+	`date` DATETIME,
+	`seatsAvailable` INT,
+
+	PRIMARY KEY (`date`)
+);
+
+CREATE TABLE `seat` (
+	`number` INT,
+
+	PRIMARY KEY (`number`)
+);
+
+/*
+ * Relationships.
+ */
+
+CREATE TABLE `can_land` (
+	`airplane_type_name` VARCHAR(64),
+	`airport_code` INT,
+
+	PRIMARY KEY (`airplane_type_name`, `airport_code`),
+
+	FOREIGN KEY (`airport_code`)
+		REFERENCES `airport` (`code`),
+
+	FOREIGN KEY (`airplane_type_name`)
+		REFERENCES `airplane_type` (`name`)
+);
+
+CREATE TABLE `type` (
+	`airplane_type_name` VARCHAR(64),
+	`airplane_id` INT,
+
+	PRIMARY KEY (`airplane_id`),
+
+	FOREIGN KEY (`airplane_type_name`)
+		REFERENCES `airplane_type` (`name`)
+		ON DELETE NO ACTION,
+
+	FOREIGN KEY (`airplane_id`)
+		REFERENCES `airplane` (`id`)
+);
+
+CREATE TABLE `departure_airport` (
+	`airport_code` INT,
+	`flight_leg_number` INT,
+	`scheduled_departure_time` DATETIME,
+
+	PRIMARY KEY (`flight_leg_number`),
+
+	FOREIGN KEY (`flight_leg_number`)
+		REFERENCES `flight_leg` (`number`),
+
+	FOREIGN KEY (`airport_code`)
+		REFERENCES `airport` (`code`)
+		ON DELETE NO ACTION
+);
+
+CREATE TABLE `arrival_airport` (
+	`airport_code` INT,
+	`flight_leg_number` INT,
+	`scheduled_arrival_time` DATETIME,
+
+	PRIMARY KEY (`flight_leg_number`),
+
+	FOREIGN KEY (`flight_leg_number`)
+		REFERENCES `flight_leg` (`number`),
+
+	FOREIGN KEY (`airport_code`)
+		REFERENCES `airport` (`code`)
+		ON DELETE NO ACTION
+);
+
+CREATE TABLE `departs` (
+	`airport_code` INT,
+	`leg_instance_date` DATETIME,
+	`depart_time` DATETIME,
+
+	PRIMARY KEY (`leg_instance_date`),
+
+	FOREIGN KEY (`airport_code`)
+		REFERENCES `airport` (`code`),
+
+	FOREIGN KEY (`leg_instance_date`)
+		REFERENCES `leg_instance` (`date`)
+);
+
+CREATE TABLE `arrives` (
+	`airport_code` INT,
+	`leg_instance_date` DATETIME,
+	`arrive_time` DATETIME,
+
+	PRIMARY KEY (`leg_instance_date`),
+
+	FOREIGN KEY (`airport_code`)
+		REFERENCES `airport` (`code`),
+
+	FOREIGN KEY (`leg_instance_date`)
+		REFERENCES `leg_instance` (`date`)
+);
+
+CREATE TABLE `assigned` (
+	`airplane_id` INT,
+	`leg_instance_date` DATETIME,
+
+	PRIMARY KEY (`leg_instance_date`),
+
+	FOREIGN KEY (`airplane_id`)
+		REFERENCES `airplane` (`id`)
+		ON DELETE NO ACTION,
+
+	FOREIGN KEY (`leg_instance_date`)
+		REFERENCES `leg_instance` (`date`)
+);
+
+CREATE TABLE `legs` (
+	`flight_leg_number` INT,
 	`flight_number` INT,
-	PRIMARY KEY (`code`, `flight_number`),
+
+	PRIMARY KEY (`flight_leg_number`),
+
 	FOREIGN KEY (`flight_number`)
-		REFERENCES `flight`
+		REFERENCES `flight` (`number`)
+		ON DELETE NO ACTION,
+
+	FOREIGN KEY (`flight_leg_number`)
+		REFERENCES `flight_leg` (`number`)
 		ON DELETE CASCADE
+);
+
+CREATE TABLE `fares` (
+	`flight_number` INT,
+	`fare_code` INT,
+
+	PRIMARY KEY (`fare_code`),
+
+	FOREIGN KEY (`fare_code`)
+		REFERENCES `fare` (`code`)
+		ON DELETE CASCADE,
+
+	FOREIGN KEY (`flight_number`)
+		REFERENCES `flight` (`number`)
+		ON DELETE NO ACTION
+);
+
+CREATE TABLE `reservation` (
+	`customer_name` VARCHAR(64),
+	`cpone` INT,
+
+	`seat_number` INT,
+	`leg_instance_date` DATETIME,
+
+
+	FOREIGN KEY (`seat_number`)
+		REFERENCES `seat` (`number`)
+		ON DELETE CASCADE,
+
+	FOREIGN KEY (`leg_instance_date`)
+		REFERENCES `leg_instance` (`date`)
+		ON DELETE NO ACTION
+);
+
+CREATE TABLE `instance_of` (
+	`flight_leg_number` INT,
+	`leg_instance_date` DATETIME,
+
+	PRIMARY KEY (`flight_leg_number`, `leg_instance_date`),
+	
+	FOREIGN KEY (`leg_instance_date`)
+		REFERENCES `leg_instance` (`date`)
+		ON DELETE CASCADE,
+
+	FOREIGN KEY (`flight_leg_number`)
+		REFERENCES `flight_leg` (`number`)
+		ON DELETE NO ACTION
 );
--- cs461/homework01/droptbl.sql
+++ cs461/homework01/droptbl.sql
@@ -1,3 +1,42 @@
+/*
+ * Delete Relationships first.
+ */
+
+DROP TABLE `reservation`;
+
+DROP TABLE `instance_of`;
+
+DROP TABLE `can_land`;
+
+DROP TABLE `fares`;
+
+DROP TABLE `legs`;
+
+DROP TABLE `arrival_airport`;
 DROP TABLE `departure_airport`;
-DROP TABLE `airport`;
+
+DROP TABLE `arrives`;
+DROP TABLE `departs`;
+
+DROP TABLE `assigned`;
+
+DROP TABLE `type`;
+
+/*
+ * Delete Entities now.
+ */
+
+DROP TABLE `fare`;
+
+DROP TABLE `flight`;
+DROP TABLE `flight_leg`;
+
 DROP TABLE `airplane`;
+
+DROP TABLE `airplane_type`;
+
+DROP TABLE `leg_instance`;
+
+DROP TABLE `seat`;
+
+DROP TABLE `airport`;
