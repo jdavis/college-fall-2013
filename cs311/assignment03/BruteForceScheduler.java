f8e92c016714aba3a40a3df8e114c3acbf720970
Josh Davis <josh@joshldavis.com>
2013-09-25 00:41:29 -0500

    Add code to generate all subsets

--- cs311/assignment03/BruteForceScheduler.java
+++ cs311/assignment03/BruteForceScheduler.java
@@ -1,9 +1,47 @@
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class BruteForceScheduler implements IScheduler {
-    private Set<IInterval>[] subsets(Set<IInterval> s) {
-        return null;
+    private Iterator<Set<IInterval>> allSubsets(Set<IInterval> s) {
+        HashSet<Set<IInterval>> result = new HashSet<Set<IInterval>>();
+        Set<IInterval> emptySet = new HashSet<IInterval>();
+
+        Iterator<IInterval> intervals = s.iterator();
+
+        /* Add the empty set */
+        result.add(emptySet);
+
+        while (intervals.hasNext()) {
+            IInterval i = intervals.next();
+            result.addAll(duplicateAndAdd(i, result));
+        }
+
+        result.remove(emptySet);
+
+        return result.iterator();
+    }
+
+    private Set<Set<IInterval>> duplicateAndAdd(IInterval i, Set<Set<IInterval>> s) {
+        HashSet<Set<IInterval>> result = new HashSet<Set<IInterval>>();
+
+        Iterator<Set<IInterval>> sets = s.iterator();
+
+        Set<IInterval> set;
+
+        while (sets.hasNext()) {
+            HashSet<IInterval> newSet = new HashSet<IInterval>();
+
+            set = sets.next();
+
+            newSet.addAll(set);
+            newSet.add(i);
+
+            result.add(newSet);
+        }
+
+        return result;
     }
 
     private boolean conflicts(Set<IInterval> s) {
@@ -13,7 +51,11 @@ public class BruteForceScheduler implements IScheduler {
     public Set<IInterval> optimalSchedule(Set<IInterval> s) {
         Set<IInterval> optimal = Collections.emptySet();
 
-        for (Set<IInterval> subset : subsets(s)) {
+        Iterator<Set<IInterval>> subsets = allSubsets(s);
+
+        while (subsets.hasNext()) {
+            Set<IInterval> subset = subsets.next();
+
             if (conflicts(subset) == false && subset.size() > optimal.size()) {
                 optimal = subset;
             }
