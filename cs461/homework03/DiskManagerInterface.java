21d41eae8958865d1ec4c616d387a658f73ad63c
Josh Davis <josh@joshldavis.com>
2013-10-21 11:11:03 -0500

    Add Page interface

--- cs461/homework03/DiskManagerInterface.java
+++ cs461/homework03/DiskManagerInterface.java
@@ -1,4 +1,8 @@
 public interface DiskManagerInterface {
+    public interface Page {
+    
+    }
+
     void allocatePage();
     void deletePage(int pid);
     void writePage(int pid, Object page);
