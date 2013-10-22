9522bc6dd2c2e87db94f8778a06575e2fecc5eef
Josh Davis <josh@joshldavis.com>
2013-10-21 20:19:26 -0500

    Add DiskManager and Database classes

---
+++ cs461/homework03/Database.java
@@ -0,0 +1,8 @@
+/**
+ * Database class for the DBMS.
+ *
+ * Contains information on how to access the database.
+ */
+public class Database {
+
+}
---
+++ cs461/homework03/DiskManager.java
@@ -0,0 +1,6 @@
+public static class DiskManager {
+    void allocatePage() {}
+    void deletePage(int pid) {}
+    void writePage(int pid, Object page) {}
+    void readPage(int pid) {}
+}
