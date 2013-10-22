51731529712ee92a1ce955383a0d94ab659d45d8
Josh Davis <josh@joshldavis.com>
2013-10-21 22:45:54 -0500

    JavaDoc PageFormat

--- cs461/homework03/PageFormat.java
+++ cs461/homework03/PageFormat.java
@@ -12,7 +12,11 @@ public interface PageFormat {
     /**
      * Reads Record bytes from an array of bytes.
      * @param bytes Array of bytes to read.
+     * @param nRecords Number of Records in the Page.
+     * @param recordLength Length of each Record.
      * @return Array of Record bytes.
      */
-    byte[][] readRecord(final byte[] bytes);
+    byte[][] readRecord(final byte[] bytes,
+            final int nRecords,
+            final int recordLength);
 }
