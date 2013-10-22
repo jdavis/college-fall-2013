628711d1176325f9038cb3f5be28c9c2241655c1
Josh Davis <josh@joshldavis.com>
2013-10-21 22:14:12 -0500

    JavaDoc PageFormat interface

--- cs461/homework03/PageFormat.java
+++ cs461/homework03/PageFormat.java
@@ -1,3 +1,18 @@
+/**
+ * Interface for arranging a series of Records on a Page.
+ */
 public interface PageFormat {
+    /**
+     * Writes an array of Record bytes to a Page.
+     * @param recordBytes Record to write.
+     * @return Array of bytes.
+     */
+    byte[] writeRecord(final byte[][] recordBytes);
 
+    /**
+     * Reads Record bytes from an array of bytes.
+     * @param bytes Array of bytes to read.
+     * @return Array of Record bytes.
+     */
+    byte[][] readRecord(final byte[] bytes);
 }
