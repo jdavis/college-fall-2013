b1786dc5a888c6fe7cce908bc94f02d1a86d5b5f
Josh Davis <josh@joshldavis.com>
2013-10-21 20:52:48 -0500

    JavaDoc DiskManager

--- cs461/homework03/DiskManager.java
+++ cs461/homework03/DiskManager.java
@@ -1,6 +1,34 @@
+/**
+ * Class that abstracts away the disk management operations.
+ */
 public static class DiskManager {
-    void allocatePage() {}
-    void deletePage(int pid) {}
-    void writePage(int pid, Object page) {}
-    void readPage(int pid) {}
+    /**
+     * Creates and returns a new Page.
+     * @return The new Page.
+     */
+    final Page createPage() {
+        return null;
+    }
+
+    /**
+     * Deletes a Page.
+     * @param pid Page id to delete.
+     */
+    final void deletePage(final int pid) { }
+
+    /**
+     * Write a Page to disk.
+     * @param pid Page ID to write.
+     * @param page Page to write.
+     */
+    final void writePage(final int pid, final Page page) { }
+
+    /**
+     * Reads a Page from disk.
+     * @param pid Page ID to read.
+     * @return Page that was read.
+     */
+    final Page readPage(final int pid) {
+        return null;
+    }
 }
