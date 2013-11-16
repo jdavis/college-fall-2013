3da28e1220a07a5df6c150e41c31910d64d34f1e
Josh Davis <josh@joshldavis.com>
2013-11-15 18:32:20 -0600

    Fix up utility.sh a bit

--- cs342/homework07/utility.sh
+++ cs342/homework07/utility.sh
@@ -1,9 +1,12 @@
+#!/bin/sh
 
 # I hate when zips extract to no folder...
 cd ..
 
+ARCHIVE=hw07-solution.zip
+
 # Create the homework submission
-zip hw7-solution.zip homework07/hw07-{env-values,interpreter,lang,tests}.rkt homework07/p5/hw07-{env-values,interpreter,lang,p5-tests}.rkt
+zip $ARCHIVE homework07/hw07-{env-values,interpreter,lang,tests}.rkt homework07/p5/hw07-{env-values,interpreter,lang,p5-tests}.rkt
 
 # Move to the solution dir
-mv hw7-solution.zip homework07
+mv hw07-solution.zip homework07
