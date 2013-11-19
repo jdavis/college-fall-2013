47209b357a677103e1ee1e88fa95114cd6a283c8
Josh Davis <josh@joshldavis.com>
2013-11-18 19:57:19 -0600

    Add empty IGraph impl

---
+++ cs311/assignment06/src/edu/iastate/cs311/f13/hw6/Graph.java
@@ -0,0 +1,29 @@
+package edu.iastate.cs311.f13.hw6;
+
+import java.util.Collection;
+
+public class Graph implements IGraph {
+    public Collection<String> getVertices() {
+        return null;
+    }
+
+    public Collection<Pair<String, String>> getOutgoingEdges(String v) {
+        return null;
+    }
+
+    public void addVertex(String v) {
+
+    }
+
+    public void addEdge(Pair<String, String> e) {
+
+    }
+
+    public void deleteVertex(String v) {
+
+    }
+
+    public void deleteEdge(Pair<String, String> e) {
+
+    }
+}
