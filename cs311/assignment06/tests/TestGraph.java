c5b1ceef1fd672a3fb48cf0886b57b1331246e1f
Josh Davis <josh@joshldavis.com>
2013-11-18 20:11:14 -0600

    Add more test cases

--- cs311/assignment06/tests/TestGraph.java
+++ cs311/assignment06/tests/TestGraph.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import edu.iastate.cs311.f13.hw6.IGraph;
@@ -26,6 +27,16 @@ public class TestGraph {
     }
 
     @Test
+    public void testEmptyGraph() {
+        IGraph g = TestRunner.newGraph();
+
+        Collection<String> expected = Arrays.asList();
+        Collection<String> actual = g.getVertices();
+
+        assertThat("Check the graph has no vertices", actual, equalTo(expected));
+    }
+
+    @Test
     public void testAddOneVertex() {
         IGraph g = TestRunner.newGraph();
         String v = "A";
@@ -35,6 +46,36 @@ public class TestGraph {
         Collection<String> expected = Arrays.asList(v);
         Collection<String> actual = g.getVertices();
 
-        assertThat("Added vertex exists", actual, equalTo(expected));
+        assertThat("Check the added vertex was added", actual, equalTo(expected));
+    }
+
+    @Test
+    public void testAddMultipleVertices() {
+        IGraph g = TestRunner.newGraph();
+        ArrayList<String> vertices = new ArrayList<String>();
+
+        for (char c = 'A'; c < 'A' + 10; c += 1) {
+            String s = "" + c;
+            vertices.add(s);
+            g.addVertex(s);
+        }
+
+        Collection<String> expected = (Collection<String>) vertices;
+        Collection<String> actual = g.getVertices();
+
+        assertThat("Check for added vertices", actual, equalTo(expected));
+    }
+
+    @Test
+    public void testEmptyEdgesForSingleVertex() {
+        IGraph g = TestRunner.newGraph();
+        String v = "A";
+
+        g.addVertex(v);
+
+        Collection<String> expected = Arrays.asList();
+        Collection<String> actual = g.getOutgoingEdges(v);
+
+        assertThat("Check a single vertex has no outgoing edges", actual, equalTo(expected));
     }
 }
