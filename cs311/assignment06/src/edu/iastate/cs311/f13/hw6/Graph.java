9e0ff3e4b890868325963d315bdadfdd4acc6b18
Josh Davis <josh@joshldavis.com>
2013-11-26 17:23:16 -0600

    Update Graph implementation, document, warnings

--- cs311/assignment06/src/edu/iastate/cs311/f13/hw6/Graph.java
+++ cs311/assignment06/src/edu/iastate/cs311/f13/hw6/Graph.java
@@ -10,21 +10,21 @@ import java.util.Map.Entry;
  * Implementation of IGraph interface.
  */
 public class Graph implements IGraph {
-    /** Hashmap of all the edges in the graph. */
+    /** Hashmap of all the vertices, edges in the graph. */
     private HashMap<String, ArrayList<Pair<String, String>>> mData;
 
     @Override
     public final String toString() {
         String result = "";
-        Iterator it = mData.entrySet().iterator();
+        Iterator<Entry<String, ArrayList<Pair<String, String>>>> it = mData.entrySet().iterator();
 
         result += "Graph:\n";
         while (it.hasNext()) {
-            Entry entry = (Entry) it.next();
-            String u = (String) entry.getKey();
+            Entry<String, ArrayList<Pair<String, String>>> entry = it.next();
+            String u = entry.getKey();
 
 
-            ArrayList<Pair<String, String>> edges = (ArrayList<Pair<String, String>>) entry.getValue();
+            ArrayList<Pair<String, String>> edges = entry.getValue();
             result += "\t" + u + " = " + edges + "\n";
         }
 
@@ -46,11 +46,14 @@ public class Graph implements IGraph {
     @Override
     public final Collection<Pair<String, String>> getOutgoingEdges(
             final String v) {
+        // Returns either null or the ArrayList of outgoing edges
         return (Collection<Pair<String, String>>) mData.get(v);
     }
 
     @Override
     public final void addVertex(final String v) {
+        // Only add if the graph doesn't contain the vertex
+        // This allows us to add vertices multiple times without consequence
         if (!mData.containsKey(v)) {
             mData.put(v, new ArrayList<Pair<String, String>>());
         }
@@ -61,25 +64,38 @@ public class Graph implements IGraph {
         String v = e.first;
         ArrayList<Pair<String, String>> edges = mData.get(v);
 
+        // Only add the edge if the originating vertex is in the graph
         if (edges != null) {
             edges.add(e);
         }
 
     }
 
+    /**
+     * Deleting a vertex takes O(V * E) time in the worst case.
+     *
+     * The algorithm needs to go through all vertices and its edges to see if
+     * there is an edge to the newly deleted vertex.
+     *
+     * @param v Edge to delete
+     */
     @Override
     public final void deleteVertex(final String v) {
+        // First remove the ArrayList
         mData.remove(v);
 
-        Iterator it = mData.entrySet().iterator();
+        // Iterate over all the key value pairs
+        Iterator<Entry<String, ArrayList<Pair<String, String>>>> it = mData.entrySet().iterator();
 
         while (it.hasNext()) {
-            Entry entry = (Entry) it.next();
-            String u = (String) entry.getKey();
+            Entry<String, ArrayList<Pair<String, String>>> entry = it.next();
 
+            String u = entry.getKey();
             Pair<String, String> e = new Pair<String, String>(u, v);
 
-            ArrayList<Pair<String, String>> edges = (ArrayList<Pair<String, String>>) entry.getValue();
+            ArrayList<Pair<String, String>> edges = entry.getValue();
+
+            // Delete the edge if it exists in the outgoing edges
             if (edges.contains(e)) {
                 edges.remove(e);
             }
