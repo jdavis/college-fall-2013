51b08625652d7584064a2ca7837aa42fb6dd4051
Josh Davis <josh@joshldavis.com>
2013-12-11 12:53:25 -0600

    Add debugging code for garbage collection

--- cs342/homework09/problem2/test-runner.rkt
+++ cs342/homework09/problem2/test-runner.rkt
@@ -4,3 +4,18 @@
 
 ; Run all the tests
 (test-all)
+
+(#%require "hw09-env-values.rkt")
+(initialize-store!)
+(displayln (~a "Blank Store: " the-store))
+(define env (extend-env-wrapper 'x 1 (empty-env) #f))
+(displayln (~a "Store 1: " the-store))
+(newref 2)
+(displayln (~a "Store 2: " the-store))
+(newref 3)
+(displayln (~a "Store 3: " the-store))
+(displayln (~a "The store before Mark: " the-store))
+(mark-all)
+(displayln (~a "The store after Mark: " the-store))
+(clear-reachable env)
+(displayln (~a "The store after Clear-reachable " the-store))
