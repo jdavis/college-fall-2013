d8d9d59c67420d2e67eca549486322a43f16d4b3
Josh Davis <josh@joshldavis.com>
2013-12-13 22:43:07 -0600

    Add all suite tests

--- cs342/homework09/problem3/test-runner.rkt
+++ cs342/homework09/problem3/test-runner.rkt
@@ -3,4 +3,15 @@
 (#%require "hw09-tests.rkt")
 
 ; Run all the tests
-(test suite-point)
+;(test suite-num)
+;(test suite-step)
+;(test suite-point)
+;(test suite-add)
+;(test suite-origin)
+(test suite-if)
+;(test suite-move)
+;(test suite-val)
+;(test suite-fun)
+;(test suite-call)
+;(test suite-block)
+(test suite-def)
